f4a18a73fbba20b58662796405b89ea5b9a62642
Raúl García-Verdugo Cardador <60638702+Zerronir@users.noreply.github.com>
2022-01-18 20:09:13 +0100

    All

---
+++ src/components/CountrySelect.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <select @change="onChange()"
+         v-model="selected" class="form-select mt-10 block w-full p-3 rounded border-solid border-2 border-blue-900 mb-10">
+            <option value="0">Select country</option>
+            <option v-for="country in countries" :value="country.ID">
+                {{ country.Country }}
+            </option>
+        </select>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'CountrySelect',
+    props: ['countries'],
+    data() {
+        return {
+            selected: 0
+        }
+    },
+    methods: {
+        onChange() {
+            const con = this.countries.find((x) => x.ID === this.selected);
+            console.log(con)
+            this.$emit('get-country', con)
+        }
+    }
+}
+</script>
---
+++ src/components/DataBoxes.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="grid md:grid-cols-2">
+        <div class="shadow-md text-center m-3 rounded bg-blue-100 p-10">
+            <h3 class="text-3xl text-blue-900 font-bold mb-4">Cases</h3>
+            <div class="text-2xl mb-4">
+                <span class="font-bold">New:</span>
+                {{ addComma(stats.NewConfirmed) }}
+            </div>
+
+            <div class="text-2xl mb-4">
+                <span class="font-bold">Total:</span>
+                {{ addComma(stats.TotalConfirmed) }}
+            </div>
+        </div>
+
+        <div class="shadow-md text-center rounded m-3 bg-yellow-100 p-10">
+            <h3 class="text-3xl text-yellow-900 font-bold mb-4">Deaths:</h3>
+            <div class="text-2xl mb-4">
+                <span class="font-bold">New:</span>
+                {{ addComma(stats.NewDeaths) }}
+            </div>
+
+            <div class="text-2xl mb-4">
+                <span class="font-bold">Total:</span>
+                {{ addComma(stats.TotalDeaths) }}
+            </div>
+        </div>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'DataBoxes',
+    props: ['stats'],
+    methods: {
+        addComma(n) {
+            return n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.');
+        }
+    }
+}
+</script>
